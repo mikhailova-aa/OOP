02dff8be561ada9ff43248c28bc8973a7cdd736f
mikhailova-aa <mikhailova.aa@phystech.edu>
2022-04-27 12:37:53 +0300

    changed

--- deadline.h
+++ deadline.h
@@ -2,6 +2,8 @@
 #define DEADLINE_H
 #include <string>
 #include <iostream>
+#include <list>
+using std::list;
 
 void test_deadline();
 void test_periodical();
@@ -9,13 +11,15 @@ int Deadline();
 using namespace std;
 
 class DeadlineTask{
+
+public: 
+
     string id; //string identifier 
     string task; //task
     string name; //executor
     int imp; // level of importance, on a scale of 1 to 3
     int time; // deadline timestamp 
 
-public:
 
 // creating task with params
 
@@ -40,27 +44,19 @@ bool change_time(int dt);
 };
 
 
-class PeriodicalTask{
-	string id; //string identifier 
-	string task; //task
-	string name; //executor
-	int imp; // level of importance, on a scale of 1 to 3
-	int time; // deadline timestamp 
-	int period; // number of seconds between nearest execurion
-	
+
+class PeriodicalTask : public DeadlineTask
+{
+
 public:
+	
+int period; // number of seconds between nearest execurion	
 
 // creating task with params
 
 PeriodicalTask(const string& id, const string& task, const string& name, int imp, int time, int period)
     :id(id), task(task), name(name), imp(imp), time(time), period(period){}
     
-int get_imp() const {
-    return imp;
-  }
-int get_time() const {
-    return time;
-  }
 int get_period() const{
 	return period;
 }
@@ -68,6 +64,46 @@ int get_period() const{
 
 bool skip_execution();
 };
+
+// должен быть отсортирован по времени (т.е. в начале стоят те задания, что должны быть выполнены раньше остальных
+class Plan
+{
+public:
+
+DeadlineTask *task;
+Plan *next;
+
+Plan () {this->next = NULL;}
+Plan(DeadlineTask *newtask)
+{
+	this->task = newtask;
+	this->next = NULL;
+}
+
+};
+
+class Planner
+{ 
+Plan *head;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #endif // DEADLINE_H
